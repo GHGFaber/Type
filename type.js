8a6f6f4937773cbf65d34ce9a63a63a901ee9031
GHGFaber <moises.fuentes904@gmail.com>
2023-02-06 11:12:31 -0800

    api check

--- type.js
+++ type.js
@@ -1,3 +1,15 @@
-fetch('https://api.api-ninjas.com/v1/quotes?category=movies')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const url = "https://api.api-ninjas.com/v1/quotes?category=movies";
+fetch(url, {
+  method: "GET",
+  withCredentials: true,
+  headers: {
+    "X-Auth-Token": "xCy2+7K4+sCujiFJWTqYMg==KLgD8GTdYqlJ5WCE"
+  }
+})
+  .then(resp => resp.json())
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
