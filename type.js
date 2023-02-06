b273ef0a6f4f4c93a4080ba465b540823880702d
GHGFaber <moises.fuentes904@gmail.com>
2023-02-06 13:15:10 -0800

    getting quote api

--- type.js
+++ type.js
@@ -1,16 +1,17 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fea301b3a8mshb2daee565fe6347p10e567jsn442367504eaa',
+		'X-RapidAPI-Host': 'quotes-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
 fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
 async function getQuote(){
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': 'fea301b3a8mshb2daee565fe6347p10e567jsn442367504eaa',
-			'X-RapidAPI-Host': 'quotes-by-api-ninjas.p.rapidapi.com'
-		}
-	};
     const response =  await fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options);
 	const json = await response.json();
 	const { quote } = json;
