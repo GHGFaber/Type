beecb2b848484c1a42f2714b6353ed601cebe308
GHGFaber <66189892+GHGFaber@users.noreply.github.com>
2023-02-05 23:44:01 -0800

    testing apis

--- type.js
+++ type.js
@@ -1,13 +1,10 @@
-var category = 'movies'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
-    headers: { 'X-Api-Key': 'YOUR_API_KEY'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+(fetch('https://reqres.in/api/users')
+    .then(res => {
+        if(res.ok){
+            console.log('SUCCES')
+        } else {
+            console.log("Not Successful")
+        }
+    })
+    .then(data => console.log(data)))
+    .catch(error => console.log('ERROR'))
