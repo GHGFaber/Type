e8e7d9bef5eb961ce370e8d68c3209e303ddebea
GHGFaber <moises.fuentes904@gmail.com>
2023-02-06 11:05:55 -0800

    testing api

--- type.js
+++ type.js
@@ -1,18 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
-var category = 'movies'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
-    headers: { 'X-Api-Key': 'xCy2+7K4+sCujiFJWTqYMg==KLgD8GTdYqlJ5WCE'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+fetch('http://example.com/movies.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
