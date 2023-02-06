982321770f009081e09a1fbf41687d286b498d6e
GHGFaber <moises.fuentes904@gmail.com>
2023-02-06 13:26:10 -0800

    api quote test

--- type.js
+++ type.js
@@ -14,7 +14,7 @@ fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options)
 async function getQuote(){
     const response =  await fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options);
 	const data = await response.json();
-	const { quote } = response.data;
+	const { quote } = data;
 	console.log(quote);
 }
 
