a667726a21e0da86ae51674b441a5ef9acbc3cf4
GHGFaber <moises.fuentes904@gmail.com>
2023-02-06 13:45:42 -0800

    testing for movies category api

--- type.js
+++ type.js
@@ -6,7 +6,7 @@ const options = {
 	}
 };
 
-fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options)
+fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes?category=movies', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
