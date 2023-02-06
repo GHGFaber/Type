19efdea04110832806be30975e19660ba43c0d66
GHGFaber <moises.fuentes904@gmail.com>
2023-02-06 13:48:23 -0800

    quotes api movies

--- type.js
+++ type.js
@@ -12,6 +12,11 @@ fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes?category=movies', o
 	.catch(err => console.error(err));
 
 async function getQuote(){
+	fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes?category=movies', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+	
     const response =  await fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options);
 	const data = await response.json();
 
