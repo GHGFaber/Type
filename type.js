1da5b8036496e820a1e6ad2a747bdc37d3d061a5
GHGFaber <moises.fuentes904@gmail.com>
2023-02-06 10:59:02 -0800

    fixing conflict

--- type.js
+++ type.js
@@ -1,3 +1,18 @@
-fetch('https://api.api-ninjas.com/v1/quotes?category=movies')
-  .then((response) => response.json())
-  .then((quote) => console.log(quote));
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+var category = 'movies'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+    headers: { 'X-Api-Key': 'xCy2+7K4+sCujiFJWTqYMg==KLgD8GTdYqlJ5WCE'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
