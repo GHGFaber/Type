7ddfa87c935a71cd3adc7fe56bd8dfea9f4ea9ad
GHGFaber <moises.fuentes904@gmail.com>
2023-02-06 13:39:22 -0800

    testing api console log

--- type.js
+++ type.js
@@ -14,9 +14,8 @@ fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options)
 async function getQuote(){
     const response =  await fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options);
 	const data = await response.json();
-	const { quotes } = data;
-	console.log(quotes);
-	return data.quote
+	const { quote } = data;
+	console.log(data);
 }
 
 getQuote();
