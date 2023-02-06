f08f04fa941673bd73bf5d0db8d0620daea41e2a
GHGFaber <moises.fuentes904@gmail.com>
2023-02-06 12:32:01 -0800

    testing getting quote

--- type.js
+++ type.js
@@ -14,7 +14,8 @@ const options = {
 async function getQuote(){
     const response =  await fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options);
 	const data = await response.json();
-	console.log(data.quote);
+	const { quote } = data;
+	console.log(quote);
 }
 
 getQuote();
