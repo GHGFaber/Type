ab8401d23610db789b74ceaab8000d767b7cdbf8
GHGFaber <moises.fuentes904@gmail.com>
2023-02-05 20:12:09 -0800

    testing api

--- type.js
+++ type.js
@@ -1,3 +1,13 @@
-fetch('https://api.api-ninjas.com/v1/quotes?category=movies')
-  .then((response) => response.json())
-  .then((quote) => console.log(quote));
+var category = 'movies'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+    headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
