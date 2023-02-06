d4151fc71e6ba60d72e0dc7943bdad0fd840561f
GHGFaber <66189892+GHGFaber@users.noreply.github.com>
2023-02-05 23:51:09 -0800

    movies test api

--- type.js
+++ type.js
@@ -1,10 +1,13 @@
-(fetch('https://reqres.in/api/users')
-    .then(res => {
-        if(res.ok){
-            console.log('SUCCES')
-        } else {
-            console.log("Not Successful")
-        }
-    })
-    .then(data => console.log(data)))
-    .catch(error => console.log('ERROR'))
+var category = 'movies'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+    headers: { 'X-Api-Key': 'xCy2+7K4+sCujiFJWTqYMg==KLgD8GTdYqlJ5WCE'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
