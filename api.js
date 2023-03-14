99a1685c73be76d2de1acb4c5c1a83f28c54d6ec
GHGFaber <moises.fuentes904@gmail.com>
2023-03-14 14:56:07 -0700

    adding base code for random word typing

---
+++ api.js
@@ -0,0 +1,16 @@
+/*const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fea301b3a8mshb2daee565fe6347p10e567jsn442367504eaa',
+		'X-RapidAPI-Host': 'quotes-by-api-ninjas.p.rapidapi.com'
+	}
+};
+let quote = [];
+async function getQuote(){
+
+    const response =  await fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes?category=movies', options);
+	const data = await response.json();
+	quote = data[0].quote;
+}*/
+
+//here for the moment until implemented 
--- type.js
+++ type.js
@@ -1,28 +1,26 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'fea301b3a8mshb2daee565fe6347p10e567jsn442367504eaa',
-		'X-RapidAPI-Host': 'quotes-by-api-ninjas.p.rapidapi.com'
-	}
-};
-let quote = [];
-async function getQuote(){
+const words = '';
 
-    const response =  await fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes?category=movies', options);
-	const data = await response.json();
-	quote = data[0].quote;
-	Game();
+function addClass(el, name){
+	el.className += ' '+name;
 }
-
+function removeClass(el, name){
+	el.className = el.className.replace(name, '');
+}
 function format(text){
-	return `<div class = "word">
-	<span class = "letter">
-	${text.split('').join('</span><span class = letter>')}
-	</span></div>`;
+	return `<div class = "word"><span class = "letter">${text.split('').join('</span><span class = letter>')}</span></div>`;
 }
+
 function Game(){
 	//document.getElementById('text').innerHTML = quote;
 	document.getElementById('text').innerHTML += format(quote);
-}
+	addClass(document.querySelector('.word'), 'current');
+	addClass(document.querySelector('.letter'), 'current'); 
+	document.getElementById('Game').addEventListener('keyup', ev => {
+		const key = ev.key;
+		const currentLetter = document.querySelector('.letter.current');
+		const expected = currentLetter.innerHTML;
 
-getQuote();
+		console.log({key,expected});
+	})
+}
+Game();
